3adf93e3339a210ddabed5ea9306aabdde256c1f
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-21 03:57:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   smallest_value = 200
   name_hash.each do |name, key|
-    if name == name_hash
+    if key == name_hash
       smallest_value = 200
     end
     smallest_value
