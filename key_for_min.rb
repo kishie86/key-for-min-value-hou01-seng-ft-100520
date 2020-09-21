843949114be76d36536a444c9686a6c64320692e
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-21 16:43:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,15 @@ require "pry"
 def key_for_min_value(name_hash)
   total = 0
   smallest_value = 200
+  smallest_key_name = nil
   name_hash.each do |key, value|
-    binding.pry
+    #binding.pry
     if smallest_value > value  
       smallest_value = value
-      smallest_key_name = 
+      smallest_key_name = key
       #smallest_value 
     end
     smallest_value
 end
+smallest_key_name
 end
