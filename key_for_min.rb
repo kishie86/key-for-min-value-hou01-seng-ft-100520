09a941fb4976295a921980db61c3572606fdf959
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-21 03:27:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_value = 
+  name_hash.each do |key|
+    if key == name_hash.min 
+      smallest_value = key
+    end
+    smallest_value
+end
 end
